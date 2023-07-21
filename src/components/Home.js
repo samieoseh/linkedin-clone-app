0860df6f9679e0da08689b717050efe8deaab91a
Samuel Oseh <samieoseh@gmail.com>
2023-07-21 10:57:37 +0100

    Created Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,54 @@ import Section from './Section'
 function Home() {
   return (
     <Container>
-        <Section />
-        <Section />
+        <Section 
+          title="Model S"
+          description="Order Onliine for Touchless Delivery"
+          backgroundImg="model-s.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+        <Section 
+          title="Model Y"
+          description="Order Onliine for Touchless Delivery"
+          backgroundImg="model-y.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+        <Section 
+          title="Model 3"
+          description="Order Onliine for Touchless Delivery"
+          backgroundImg="model-3.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+        <Section 
+          title="Model X"
+          description="Order Onliine for Touchless Delivery"
+          backgroundImg="model-x.jpg"
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+        <Section 
+          title="Lowest cost solar panels in America"
+          description="Money-back guarantee"
+          backgroundImg="solar-panel.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn more"
+        />
+         <Section 
+          title="Solar for new roofs"
+          description="Money-back guarantee"
+          backgroundImg="solar-roof.jpg"
+          leftBtnText="Order now"
+          rightBtnText="Learn more"
+        />
+        <Section 
+          title="Accessories"
+          description=""
+          backgroundImg="accessories.jpg"
+          leftBtnText="Order now"
+        />
     </Container>
   )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 import { styled } from 'styled-components'
 
-function Section() {
+function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
         <ItemText>
-            <h1>Model S</h1>
-            <p>Order Onliine for Touchless Delivery</p>
+            <h1>{title}</h1>
+            <p>{description}</p>
         </ItemText>
         <Buttons>
             <ButtonGroup>
-                <LeftButton>Custom Order</LeftButton>
-                <RightButton>Existing Inventory</RightButton>
+                <LeftButton>{leftBtnText}</LeftButton>
+                { rightBtnText && <RightButton>{rightBtnText}</RightButton>}
             </ButtonGroup>
             <DownArrow src="./images/down-arrow.svg" />
         </Buttons>
@@ -25,7 +25,7 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: url('/images/model-s.jpg');
+    background-image: ${props => `url("/images/${props.bgImage}")`};
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
@@ -44,7 +44,10 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display: flex;
     margin-bottom: 30px;
-    flex-direction: column;
+    flex-direction: row;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 
 const LeftButton = styled.div`
@@ -65,7 +68,7 @@ const LeftButton = styled.div`
 
 const RightButton = styled(LeftButton)`
     background-color: #fff;
-    color: rgba(23, 26, 32, 0.8);
+    color:#111;
 `
 
 const DownArrow = styled.img`
