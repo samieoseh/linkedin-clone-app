573fb270b8cf0b2ca5fc7826fbf259971bc3abf3
Samuel Oseh <samieoseh@gmail.com>
2023-07-21 17:23:36 +0100

    Finishing Touches

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,28 +11,28 @@ function Home() {
     <Container>
         <Section 
           title="Model S"
-          description="Order Onliine for Touchless Delivery"
+          description="Order Online for Touchless Delivery"
           backgroundImg="model-s.jpg"
           leftBtnText="Custom Order"
           rightBtnText="Existing Inventory"
         />
         <Section 
           title="Model Y"
-          description="Order Onliine for Touchless Delivery"
+          description="Order Online for Touchless Delivery"
           backgroundImg="model-y.jpg"
           leftBtnText="Custom Order"
           rightBtnText="Existing Inventory"
         />
         <Section 
           title="Model 3"
-          description="Order Onliine for Touchless Delivery"
+          description="Order Online for Touchless Delivery"
           backgroundImg="model-3.jpg"
           leftBtnText="Custom Order"
           rightBtnText="Existing Inventory"
         />
         <Section 
           title="Model X"
-          description="Order Onliine for Touchless Delivery"
+          description="Order Online for Touchless Delivery"
           backgroundImg="model-x.jpg"
           leftBtnText="Custom Order"
           rightBtnText="Existing Inventory"
